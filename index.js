b77ea5e2b59bcb39af5881057b4e6c927588af14
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-11-24 22:03:24 -0800

    appli minimum (bis)

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', function (request, response){
+      response.send(cool())
+  })
+  .listen(PORT, () => console.log(`Node app is running  on ${ PORT }`))
