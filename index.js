4b3924efda922375fd7612d1d89b68013c18fe57
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-11-24 23:07:24 -0800

    test appelant le fichier index.html directement

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index.html'))
-  .get('/cool', function (request, response){
-      response.send(cool())
-  })
+  // .use(express.static(path.join(__dirname, 'public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  .get('/', (req, res) => res.sendFile(process.cwd() + '/index.html'))
+  // .get('/cool', function (request, response){
+  //     response.send(cool())
+  // })
   .listen(PORT, () => console.log(`Node app is running  on ${ PORT }`))
