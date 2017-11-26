6f329988bd46fcf9af6837fb9377eae393730c94
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-11-25 16:19:40 -0800

    debut d'alignement sur le boilerplate clementine

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var express = require('express');
+const express = require('express')
+const PORT = process.env.PORT || 5000
 
-var app = express();
-
-app.get('/', function (req, res) {
-	res.send('Bonjour le monde!');
-});
-
-app.listen(process.env.PORT || 5000, function () {
-	console.log('Listening on port 3000...');
-});
+express()
+  .get('/', (req, res) => res.sendFile(process.cwd() + '/index.html'))
+  .get('/:date', function(req, res) {
+      var d = new Date(req.params.date);
+      res.send('request params:' + d.toDateString())
+  })
+  .listen(PORT, () => console.log(`Node app is running  on ${ PORT }`))
