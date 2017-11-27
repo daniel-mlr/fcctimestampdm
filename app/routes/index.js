791bda57ed733847a126bf40e4c70748e45a73d2
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-11-26 23:34:38 -0800

    Finalisation css

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,5 @@
 // 'use strict'
 
-mess = function(datestr) {
-    if (/^\d+$/.test(datestr)) {
-        datestr = parseInt(datestr)
-    }
-    
-    var d = new Date(datestr)
-
-    if (d == 'Invalid Date') {
-        return {'unix': null, 'natural': null}
-    } else {
-        return {'unix': d.valueOf(), 'natural': d.toDateString()};
-    }
-}
-
-
 module.exports = function(app) {
     app.route('/').get( function(req, res) {
         res.sendFile(process.cwd() + '/public/index.html', function(err) {
@@ -30,4 +15,19 @@ module.exports = function(app) {
     app.route('/:f1/:f2/:f3').get( function(req, res) {
         res.json(mess(req.params.f1 + ' ' + req.params.f2 + ' ' + req.params.f3))
     })
+    
+    var mess = function(datestr) {
+        if (/^\d+$/.test(datestr)) {
+            datestr = parseInt(datestr)
+        }
+
+        var d = new Date(datestr)
+
+            if (d == 'Invalid Date') {
+                return {'unix': null, 'natural': null}
+            } else {
+                return {'unix': d.valueOf(), 'natural': d.toDateString()};
+            }
+    }
+
 }
